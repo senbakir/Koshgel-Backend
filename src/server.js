f1fa869a4cc11b088e5953503556dd3362c8a9fd
senbakir <senbakir@gmail.com>
2025-12-25 17:51:55 -0500

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,23 @@
+
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+import healthRoute from "./routes/health.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Koshgel API Running" });
+});
+
+app.use("/api", healthRoute);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
