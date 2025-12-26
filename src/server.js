f44b723325c08213c97806501b8b2bd2c473fabe
senbakir <senbakir@gmail.com>
2025-12-25 21:25:54 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,11 @@
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log("Mongo Error:", err));
 
 import express from "express";
 import cors from "cors";
