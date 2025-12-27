91e072e10f60adf8a04ae5a11ca0e360eb27715a
senbakir <senbakir@gmail.com>
2025-12-27 02:02:03 -0500

    Create User.js

---
+++ src/models/User.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullName: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: String,
+  categories: [String]
+});
+
+export default mongoose.model("User", userSchema);
