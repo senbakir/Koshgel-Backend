b21df96b6b775b35097e7a8bce6576c8d628b564
senbakir <senbakir@gmail.com>
2025-12-27 17:53:24 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,27 +3,31 @@ import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
-import meRoute from "./routes/me.js";
 
 import authRoute from "./routes/auth.js";
 import healthRoute from "./routes/health.js";
+import meRoute from "./routes/me.js";
 
 dotenv.config();
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+// Root
 app.get("/", (req, res) => {
   res.json({ message: "Koshgel API Running" });
 });
 
+// Routes
 app.use("/api/auth", authRoute);
 app.use("/api", healthRoute);
 app.use("/api", meRoute);
 
+// Mongo
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
