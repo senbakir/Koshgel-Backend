b1aee7d640abce5c2f064f6acd3c636e8b52b497
senbakir <senbakir@gmail.com>
2025-12-27 23:04:55 -0500

    Update me.js

--- src/routes/me.js
+++ src/routes/me.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { auth } from "../middleware/auth.js";
+import { requireAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/me", auth, (req, res) => {
+router.get("/", requireAuth, (req, res) => {
   res.json({ ok: true, user: req.user });
 });
 
