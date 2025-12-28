8d1e95b027fa448d1ff4986d7f5a38ce368b6e16
senbakir <senbakir@gmail.com>
2025-12-28 11:48:45 -0500

    Update admin.js

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,3 +1,5 @@
+console.log("ADMIN MONGODB_URI:", process.env.MONGODB_URI?.slice(0, 40));
+console.log("Mongoose state:", require("mongoose").connection.readyState);
 import express from "express";
 import { requireAuth, requireAdmin } from "../middleware/auth.js";
 
