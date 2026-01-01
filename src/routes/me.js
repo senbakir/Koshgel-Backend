9256f3f7fbd0cde8c27bd428625d74979b366af5
senbakir <senbakir@gmail.com>
2026-01-01 16:20:50 -0500

    Update me.js

--- src/routes/me.js
+++ src/routes/me.js
@@ -3,7 +3,6 @@ import { requireAuth } from "../middleware/auth.js";
 
 const router = Router();
 
-// GET /api/me
 router.get("/me", requireAuth, (req, res) => {
   res.json({ ok: true, user: req.user });
 });
